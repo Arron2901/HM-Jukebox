4f172b5aede5180cf40fe91d169edf7970011517
ogerrard <ogerrard@cisco.com>
2025-11-10 23:25:29 -0800

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
 import Login from "./pages/Login";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Remote from "./pages/Remote";
 import { BACKEND_URL } from "./api/spotifyAPI";
 
